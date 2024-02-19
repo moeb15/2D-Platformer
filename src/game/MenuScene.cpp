70cfd5bef818cc53706c7af964eb7ed349a6fed1
Mujtaba Bader <ma2bader@uwaterloo.ca>
2024-02-19 08:18:06 -0500

    added play option, naviagates to level select scene

--- src/game/MenuScene.cpp
+++ src/game/MenuScene.cpp
@@ -3,6 +3,7 @@
 #include "engine/GameEngine.h"
 #include "game/EditorScene.h"
 #include "game/CustomLevelScene.h"
+#include "game/LevelSelectScene.h"
 
 MenuScene::MenuScene(GameEngine* gameEngine):
 	Scene(gameEngine),
@@ -23,17 +24,17 @@ void MenuScene::init() {
 	m_GameEngine->getAssets().addTexture(Textures::Menu, "graphics/menuScreen.png");
 	m_Background.setTexture(m_GameEngine->getAssets().getTexture(Textures::Menu));
 
-	sf::Text level1;
-	sf::Text level2;
-	sf::Text level3;
+	sf::Text play;
+	//sf::Text level2;
+	//sf::Text level3;
 	sf::Text customLevels;
 	sf::Text levelEditor;
 
 	m_Title.setFont(m_GameEngine->getAssets().getFont(Fonts::Main));
 	m_Subtext.setFont(m_GameEngine->getAssets().getFont(Fonts::Main));
-	level1.setFont(m_GameEngine->getAssets().getFont(Fonts::Main));
-	level2.setFont(m_GameEngine->getAssets().getFont(Fonts::Main));
-	level3.setFont(m_GameEngine->getAssets().getFont(Fonts::Main));
+	play.setFont(m_GameEngine->getAssets().getFont(Fonts::Main));
+	//level2.setFont(m_GameEngine->getAssets().getFont(Fonts::Main));
+	//level3.setFont(m_GameEngine->getAssets().getFont(Fonts::Main));
 	customLevels.setFont(m_GameEngine->getAssets().getFont(Fonts::Main));
 	levelEditor.setFont(m_GameEngine->getAssets().getFont(Fonts::Main));
 
@@ -50,27 +51,27 @@ void MenuScene::init() {
 		m_GameEngine->getWindow().getSize().y * 9 / 10
 	));
 
-	level1.setCharacterSize(50);
-	level2.setCharacterSize(50);
-	level3.setCharacterSize(50);
+	play.setCharacterSize(50);
+	//level2.setCharacterSize(50);
+	//level3.setCharacterSize(50);
 	customLevels.setCharacterSize(50);
 	levelEditor.setCharacterSize(50);
 
-	level1.setString("LEVEL 1");
-	level2.setString("LEVEL 2");
-	level3.setString("LEVEL 3");
+	play.setString("PLAY");
+	//level2.setString("LEVEL 2");
+	//level3.setString("LEVEL 3");
 	customLevels.setString("CUSTOM LEVELS");
 	levelEditor.setString("LEVEL EDITOR");
 
-	level1.setPosition(25, 100);
-	level2.setPosition(25, 150);
-	level3.setPosition(25, 200);
-	customLevels.setPosition(25, 250);
-	levelEditor.setPosition(25, 300);
+	play.setPosition(25, 100);
+	//level2.setPosition(25, 150);
+	//level3.setPosition(25, 200);
+	customLevels.setPosition(25, 150);
+	levelEditor.setPosition(25, 200);
 
-	m_Options.push_back(level1);
-	m_Options.push_back(level2);
-	m_Options.push_back(level3);
+	m_Options.push_back(play);
+	//m_Options.push_back(level2);
+	//m_Options.push_back(level3);
 	m_Options.push_back(customLevels);
 	m_Options.push_back(levelEditor);
 
@@ -97,22 +98,22 @@ void MenuScene::sDoAction(const Action& action){
 			m_OptionIndex = (m_OptionIndex + 1) % m_Options.size();
 		}
 		if (action.getName() == Actions::Select) {
-			if (m_OptionIndex >= 0 && m_OptionIndex <= 2) {
-				std::shared_ptr<GameScene> gameScene(
-					new GameScene(m_GameEngine, m_LevelPaths[m_OptionIndex])
+			if (m_OptionIndex >= 0 && m_OptionIndex == 0) {
+				std::shared_ptr<LevelSelectScene> gameScene(
+					new LevelSelectScene(m_GameEngine, "levels/rooms/levelSelect.txt")
 				);
 
-				m_GameEngine->changeScene(Scenes::Game,
+				m_GameEngine->changeScene(Scenes::LevelSelect,
 					std::static_pointer_cast<Scene>(gameScene));
 			}
-			if (m_OptionIndex == 3) {
+			if (m_OptionIndex == 1) {
 				std::shared_ptr<CustomLevelScene> customLvls(
 					new CustomLevelScene(m_GameEngine));
 
 				m_GameEngine->changeScene(Scenes::CustomLevels,
 					std::static_pointer_cast<Scene>(customLvls));
 			}
-			if (m_OptionIndex == 4) {
+			if (m_OptionIndex == 2) {
 				std::shared_ptr<EditorScene> lvlEditor(
 					new EditorScene(m_GameEngine));
 
